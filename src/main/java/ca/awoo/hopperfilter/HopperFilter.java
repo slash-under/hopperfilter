7c47c831172045d545544a5c6a036864034d35c9
Rebecca Johns <63025235+slash-under@users.noreply.github.com>
2023-10-24 22:06:53 +0200

    Update HopperFilter.java

--- src/main/java/ca/awoo/hopperfilter/HopperFilter.java
+++ src/main/java/ca/awoo/hopperfilter/HopperFilter.java
@@ -1,9 +1,12 @@
 package ca.awoo.hopperfilter;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public final class HopperFilter extends JavaPlugin {
+
+    static Logger loggerInstance;
 
     @Override
     public void onEnable() {
